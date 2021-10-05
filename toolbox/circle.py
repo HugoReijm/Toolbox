b2c8d30e3219fa281b5701ef5b71cb8c6345526f
HugoReijm <itsanickel@yahoo.com>
2021-10-05 15:06:04 -0400

    Create circle.py

---
+++ toolbox/circle.py
@@ -0,0 +1,59 @@
+import math
+import numpy as np
+import numpy.linalg as npla
+from point import point
+import matplotlib.colors as pltcolors
+
+class circle(object):
+    def __init__(self,center,radius):
+        self.center=center
+        self.radius=radius
+        self.area=math.pi*self.radius**2
+        self.circumference=2*math.pi*self.radius
+        
+    def is_circle(self,circVar,errtol=1e-12):
+        return self.center.is_point(circVar.center,errtol=errtol) and abs(self.radius-circVar.radius)<=abs(errtol)
+    
+    def inCircle(self,pointVar,includeboundary=True,errtol=1e-12):
+        p1=point(self.radius+self.center.x,self.center.y)
+        p2=point(self.radius-self.center.x,self.center.y)
+        p3=point(self.center.x,self.radius+self.center.y)
+        if (p2.x-p1.x)*(p3.y-p2.y)-(p2.y-p1.y)*(p3.x-p2.x)>0:
+            res=npla.det([[p1.x-pointVar.x,p2.x-pointVar.x,p3.x-pointVar.x],
+                          [p1.y-pointVar.y,p2.y-pointVar.y,p3.y-pointVar.y],
+                          [(p1.x-pointVar.x)**2+(p1.y-pointVar.y)**2,(p2.x-pointVar.x)**2+(p2.y-pointVar.y)**2,(p3.x-pointVar.x)**2+(p3.y-pointVar.y)**2]])
+        else:
+            res=npla.det([[p1.x-pointVar.x,p3.x-pointVar.x,p2.x-pointVar.x],
+                          [p1.y-pointVar.y,p3.y-pointVar.y,p2.y-pointVar.y],
+                          [(p1.x-pointVar.x)**2+(p1.y-pointVar.y)**2,(p3.x-pointVar.x)**2+(p3.y-pointVar.y)**2,(p2.x-pointVar.x)**2+(p2.y-pointVar.y)**2]])
+        if includeboundary:
+            return res>=-abs(errtol)
+        else:
+            return res>abs(errtol)
+        
+    def __repr__(self):
+        return "Circle <"+self.center.__repr__()+", %0.3f>"%self.radius
+        
+    def copy(self):
+        return circle(self.center.copy(),self.radius)
+    
+    def draw(self,plotaxis,center=True,color="black",alpha=1):
+        theta=np.linspace(0,2*math.pi,100)
+        if sum(pltcolors.to_rgb(color))<=1.0:
+            plotaxis.fill(self.radius*np.cos(theta)+self.center.x,self.radius*np.sin(theta)+self.center.y,
+                          facecolor=color,edgecolor="white",alpha=alpha)
+            if center:
+                plotaxis.scatter([self.center.x],[self.center.y],facecolor=color,edgecolor="white",alpha=alpha,zorder=1)
+        else:
+            plotaxis.fill(self.radius*np.cos(theta)+self.center.x,self.radius*np.sin(theta)+self.center.y,
+                          facecolor=color,edgecolor="black",alpha=alpha)
+            if center:
+                plotaxis.scatter([self.center.x],[self.center.y],facecolor=color,edgecolor="black",alpha=alpha,zorder=1)
+            
+    def kill(self,C=None):
+        if isinstance(C,list):
+            try:
+                C.remove(self)
+            except ValueError:
+                pass
+        del self
