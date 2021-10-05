d4fe498a0c9b3ae0335ae465aa6c11cc5598b904
HugoReijm <itsanickel@yahoo.com>
2021-10-05 14:54:46 -0400

    Update __init__.py

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,4 +1,7 @@
 import toolbox.continuoustoolbox
 import toolbox.discretetoolbox
+import toolbox.evolutiontoolbox 
+import toolbox.neuraltoolbox
+import toolbox.swarmtoolbox
 import toolbox.generaltoolbox
 import toolbox.matrixtoolbox
