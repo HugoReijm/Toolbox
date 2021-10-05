6552752e66f9be8bb58a845a8687c915aab0a39f
HugoReijm <itsanickel@yahoo.com>
2021-10-05 15:38:24 -0400

    Added documentation

--- toolbox/edge.py
+++ toolbox/edge.py
@@ -4,6 +4,7 @@ from point import point
 from circle import circle
 import matplotlib.colors as pltcolors
 
+#This file contains the edge class and its various methods.
 class edge(object):
     def __init__(self,p1,p2):
         self.points=[p1,p2]
@@ -15,21 +16,27 @@ class edge(object):
         self.circumCircle=None
         
     def is_edge(self,e,errtol=1e-12):
+        #This method compares whether two edges are equal or not.
         return isinstance(e,edge) and ((self.points[0].is_point(e.points[0],errtol=errtol) and self.points[1].is_point(e.points[1],errtol=errtol)) or (self.points[0].is_point(e.points[1],errtol=errtol) and self.points[1].is_point(e.points[0],errtol=errtol)))
         
     def update(self):
+        #For each end-point of the edge object, this method adds the edge to the point's list of edges it is a constituent of.
         for p in self.points:
             p.edges.append(self)
         
     def length(self):
+        #This method computes the length of the edge object and makes the length variable a singleton.
         if self.edgelength is None:
             self.edgelength=math.sqrt((self.points[0].x-self.points[1].x)**2+(self.points[0].y-self.points[1].y)**2)
         return self.edgelength
     
     def cross(self,edge):
+        #This method returns the 2-dimensional "cross product" of the edge object and another inputted edge.
         return (self.points[1].x-self.points[0].x)*(edge.points[1].y-edge.points[0].y)-(self.points[1].y-self.points[0].y)*(edge.points[1].x-edge.points[0].x)
     
     def point_edge_intersect(self,pointVar,includeboundary=True,errtol=1e-12):
+        #This method returns whether an inputted point lies on the edge object or not.
+        #Distinction is made whether to include the boundaries of the edge or not.
         if includeboundary:
             if abs((self.points[1].x-self.points[0].x)*(pointVar.y-self.points[0].y)-(pointVar.x-self.points[0].x)*(self.points[1].y-self.points[0].y))<abs(errtol):
                 if ((self.points[1].x-self.points[0].x)**2+(self.points[1].y-self.points[0].y)**2-(pointVar.x-self.points[0].x)**2-(pointVar.y-self.points[0].y)**2>=-abs(errtol)
@@ -43,6 +50,9 @@ class edge(object):
         return False
 
     def edge_edge_intersect(self,edgeVar,includeboundary=True,boolean=True,errtol=1e-12):
+        #This method returns whether the edge object intersects the other inputted edge.
+        #Distinction is made whether to include the boundaries of the edges or not.
+        #If the edges do intersect, the method can either return true or false, or return the intersection. 
         if self.is_edge(edgeVar):
             if boolean:
                 return True
@@ -156,12 +166,15 @@ class edge(object):
         return None
     
     def circumcircle(self):
+        #This method returns the smallest circumcircle of the edge object and makes the circumcircl object a singleton.
         if self.circumCircle is None:
             self.circumCircle=circle(point((self.points[0].x+self.points[1].x)/2,(self.points[0].y+self.points[1].y)/2),
                                      math.sqrt((self.points[1].x-self.points[0].x)**2+(self.points[1].y-self.points[0].y)**2)/2)
         return self.circumCircle
     
     def inCircumcircle(self,pointVar,includeboundary=True,errtol=1e-12):
+        #This method calculates whether an inputted point lies inside of the edge object's circumcircle.
+        #Distinction is made whether to include the boundary of the circumcle or not.
         pointd=point((self.points[0].x+self.points[1].x)/2,(self.points[0].y+self.points[1].y)/2)
         pointc=point(self.points[0].y-pointd.y+pointd.x,pointd.x-self.points[0].x+pointd.y)
         if (self.points[1].x-self.points[0].x)*(pointc.y-self.points[1].y)-(self.points[1].y-self.points[0].y)*(pointc.x-self.points[1].x)>0:
@@ -178,14 +191,18 @@ class edge(object):
             return res>abs(errtol)
     
     def swap(self):
+        #This method swaps the local indexes of the end-points of the edge object.
         tempP=self.points[0]
         self.points[0]=self.points[1]
         self.points[1]=tempP
     
     def __repr__(self):
+        #This method returns a string representation of the edge object.
         return "Edge <"+self.points[0].__repr__()+", "+self.points[1].__repr__()+">"
     
     def copy(self,blank=True):
+        #This method returns a copy of the edge object.
+        #Whether the copy contains all variables of the original used for constructing triangulations is user-designated.
         e=edge(self.points[0].copy(),self.points[1].copy())
         if self.edgelength is not None:
             e.edgelength=self.edgelength
@@ -197,6 +214,7 @@ class edge(object):
         return e
 
     def draw(self,plotaxis,points=True,color="black",alpha=1):
+        #This method plots the edge object into an inputted figure axis object. 
         plotaxis.plot([self.points[0].x,self.points[1].x],
                       [self.points[0].y,self.points[1].y],color=color,alpha=alpha)
         if points:
@@ -208,6 +226,9 @@ class edge(object):
                                  [self.points[0].y,self.points[1].y],facecolor=color,edgecolor="black",alpha=alpha)
         
     def kill(self,E=None):
+        #This method removes the edge object from any of its point's edge lists.
+        #This method also removes the edge object from any of its triangle's edge lists.
+        #Finally this method removes the edge object from any inputted list, then deletes itself.
         for p in self.points:
             try:
                 p.edges.remove(self)
