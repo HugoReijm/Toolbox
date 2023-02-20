97ff238826eaac4c5eae3121e2127b5ac141d651
HugoReijm <itsanickel@yahoo.com>
2023-02-20 16:41:51 -0500

    Update point.py

--- toolbox/point.py
+++ toolbox/point.py
@@ -1,5 +1,4 @@
-import math
-import matplotlib.colors as pltcolors
+import numpy as np
 
 #This file contains the point class and its various methods.
 class point(object):
@@ -11,41 +10,33 @@ class point(object):
         #This list keeps track of which triangles the point object is a constituent of.
         self.triangles=[]
         self.index=-1
+        self.value=None
     
     def is_point(self,p,errtol=1e-12):
         #This method compares whether two points are equal or not.
         return isinstance(p,point) and abs(self.x-p.x)<abs(errtol) and abs(self.y-p.y)<abs(errtol)
     
+    def __add__(self,p):
+        return point(self.x+p.x,self.y+p.y)
+    
+    def __sub__(self,p):
+        return point(self.x-p.x,self.y-p.y)
+    
+    def __mul__(self,p):
+        return point(self.x*p.x,self.y*p.y)
+    
+    def __truediv__(self,p):
+        return point(self.x/p.x,self.y/p.y)
+    
     def distance(self,p):
         #This method computes the distance between the point object and the other inputted point.
-        return math.sqrt((self.x-p.x)**2+(self.y-p.y)**2)
+        return np.sqrt((self.x-p.x)**2+(self.y-p.y)**2)
     
     def __repr__(self):
         #This method returns a string representation of the point object.
-        return "Point ("+str(round(self.x,6))+", "+str(round(self.y,6))+")"
+        return "Point "+str(self.index)+": ("+str(round(self.x,6))+", "+str(round(self.y,6))+")"
               
     def copy(self):
         #This method returns an exact copy of the point object.
         #The objects edge and triangle lists are not copied.
         return point(self.x,self.y)
-    
-    def kill(self,P=None):
-        #This method removes the point object from any of its edge's point lists.
-        #This method also removes the point object from any of its triangle's point lists.
-        #Finally this method removes the point object from any inputted list, then deletes itself.
-        for e in self.edges:
-            try:
-                e.points.remove(self)
-            except ValueError:
-                pass
-        for t in self.triangles:
-            try:
-                t.points.remove(self)
-            except ValueError:
-                pass
-        if isinstance(P,list):
-            try:
-                P.remove(self)
-            except ValueError:
-                pass
-        del self
