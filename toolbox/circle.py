af8609e1589453261fddd85bab83ab35983d79e4
HugoReijm <itsanickel@yahoo.com>
2023-02-20 16:41:08 -0500

    Update circle.py

--- toolbox/circle.py
+++ toolbox/circle.py
@@ -1,7 +1,4 @@
-import math
 import numpy as np
-import numpy.linalg as npla
-from point import point
 import matplotlib.colors as pltcolors
 
 #This file contains the circle class and its various methods.
@@ -9,62 +6,57 @@ class circle(object):
     def __init__(self,center,radius):
         self.center=center
         self.radius=radius
-        self.area=math.pi*self.radius**2
-        self.circumference=2*math.pi*self.radius
+        self.area=np.pi*self.radius**2
+        self.circumference=2*np.pi*self.radius
+        self.index=-1
         
-    def is_circle(self,circVar,errtol=1e-12):
+    def is_circle(self,c,errtol=1e-12):
         #This method compares whether two circles are equal or not.
-        return self.center.is_point(circVar.center,errtol=errtol) and abs(self.radius-circVar.radius)<=abs(errtol)
+        return self.center.is_point(c.center,errtol=errtol) and abs(self.radius-c.radius)<=abs(errtol)
     
-    def inCircle(self,pointVar,includeboundary=True,errtol=1e-12):
+    def inCircle(self,p,includeboundary=True,errtol=1e-12):
         #This method checks if an inputted point is in the circle or not.
+        """
         p1=point(self.radius+self.center.x,self.center.y)
         p2=point(self.radius-self.center.x,self.center.y)
         p3=point(self.center.x,self.radius+self.center.y)
         if (p2.x-p1.x)*(p3.y-p2.y)-(p2.y-p1.y)*(p3.x-p2.x)>0:
-            res=npla.det([[p1.x-pointVar.x,p2.x-pointVar.x,p3.x-pointVar.x],
-                          [p1.y-pointVar.y,p2.y-pointVar.y,p3.y-pointVar.y],
-                          [(p1.x-pointVar.x)**2+(p1.y-pointVar.y)**2,
-                           (p2.x-pointVar.x)**2+(p2.y-pointVar.y)**2,
-                           (p3.x-pointVar.x)**2+(p3.y-pointVar.y)**2]])
+            res=det([[p1.x-p.x,p2.x-p.x,p3.x-p.x],
+                     [p1.y-p.y,p2.y-p.y,p3.y-p.y],
+                     [(p1.x-p.x)**2+(p1.y-p.y)**2,
+                      (p2.x-p.x)**2+(p2.y-p.y)**2,
+                      (p3.x-p.x)**2+(p3.y-p.y)**2]])
         else:
-            res=npla.det([[p1.x-pointVar.x,p3.x-pointVar.x,p2.x-pointVar.x],
-                          [p1.y-pointVar.y,p3.y-pointVar.y,p2.y-pointVar.y],
-                          [(p1.x-pointVar.x)**2+(p1.y-pointVar.y)**2,
-                           (p3.x-pointVar.x)**2+(p3.y-pointVar.y)**2,
-                           (p2.x-pointVar.x)**2+(p2.y-pointVar.y)**2]])
+            res=det([[p1.x-p.x,p3.x-p.x,p2.x-p.x],
+                     [p1.y-p.y,p3.y-p.y,p2.y-p.y],
+                     [(p1.x-p.x)**2+(p1.y-p.y)**2,
+                      (p3.x-p.x)**2+(p3.y-p.y)**2,
+                      (p2.x-p.x)**2+(p2.y-p.y)**2]])
         if includeboundary:
             return res>=-abs(errtol)
         else:
             return res>abs(errtol)
-        
+        """
+        if includeboundary and (p.x-self.center.x)**2+(p.y-self.center.y)**2<=(self.radius+abs(errtol))**2:
+            return True
+        elif not includeboundary and (p.x-self.center.x)**2+(p.y-self.center.y)**2<max(self.radius-abs(errtol),0)**2:
+            return True
+        return False
+
     def __repr__(self):
         #This method returns a string representation of the circle object.
-        return "Circle <"+self.center.__repr__()+", %0.3f>"%self.radius
+        return "Circle <"+self.center.__repr__()+", %0.6f>"%self.radius
         
     def copy(self):
         #This method returns an exact copy of the circle object.
         return circle(self.center.copy(),self.radius)
     
-    def draw(self,plotaxis,center=True,color="black",alpha=1):
+    def draw(self,plotaxis,color="black",alpha=1):
         #This method plots the circle object into an inputted figure axis object. 
-        theta=np.linspace(0,2*math.pi,100)
+        theta=np.linspace(0,2*np.pi,100)
         if sum(pltcolors.to_rgb(color))<=1.0:
             plotaxis.fill(self.radius*np.cos(theta)+self.center.x,self.radius*np.sin(theta)+self.center.y,
                           facecolor=color,edgecolor="white",alpha=alpha)
-            if center:
-                plotaxis.scatter([self.center.x],[self.center.y],facecolor=color,edgecolor="white",alpha=alpha,zorder=1)
         else:
             plotaxis.fill(self.radius*np.cos(theta)+self.center.x,self.radius*np.sin(theta)+self.center.y,
                           facecolor=color,edgecolor="black",alpha=alpha)
-            if center:
-                plotaxis.scatter([self.center.x],[self.center.y],facecolor=color,edgecolor="black",alpha=alpha,zorder=1)
-            
-    def kill(self,C=None):
-        #This method removes the circle object from any inputted list, then deletes itself.
-        if isinstance(C,list):
-            try:
-                C.remove(self)
-            except ValueError:
-                pass
-        del self
